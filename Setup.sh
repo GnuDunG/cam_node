c2751b149b979af509fd652f000766099c9a5311
GnuDunG <martinschmied@gmail.com>
2017-01-19 23:11:03 +0100

    two files added

--- Setup.sh
+++ Setup.sh
@@ -10,9 +10,9 @@ git clone https://github.com/GnuDunG/cam_node
 cd cam_node
 make -f Makefile
 cp cam_node $autostart_path
-cp autostart.sh $autostart_path
+cp autostart_node.sh $autostart_path
 
 echo "reboot in 10 seconds..."
 
-sleep(10)
+#sleep(10)
 
