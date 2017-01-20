58a97d6474a79ce9f2a036e824a3b17b3b6601fb
GnuDunG <martinschmied@gmail.com>
2017-01-20 01:29:02 +0100

    two files added

--- Setup.sh
+++ Setup.sh
@@ -6,7 +6,7 @@ sudo git clone https://github.com/GnuDunG/cam_node
 cd /cam_node
 make -f Makefile
 
-#sed 's/^exit0:.*$//' /etc/rc.local 
+sed -ie 's/exit 0/\\\/cam_node\\\/cam_node/g' /etc/rc.local 
 #sed '$a sudo ' Textdatei 
 #cp autostart_node.sh /etc/rc3.d
 
