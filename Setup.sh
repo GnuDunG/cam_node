9876302b52495ab3e3125e93fc6a9dd25ff3246c
GnuDunG <martinschmied@gmail.com>
2017-01-20 02:50:12 +0100

    two files added

--- Setup.sh
+++ Setup.sh
@@ -6,11 +6,15 @@ sudo git clone https://github.com/GnuDunG/cam_node
 cd /cam_node
 make -f Makefile
 
-sed -ie 's/exit 0/\\\/cam_node\\\/cam_node/g' /etc/rc.local 
-#sed '$a sudo ' Textdatei 
+sed -i '1 a /cam_node/cam_node' /etc/rc.local
+
+#sed -ie 's/exit 0//cam_node/g' /etc/rc.local 
+#sed '20 a\cam_node' 
+#'/\\/cam_node\\/cam_node/a \exit 0' /etc/rc.local
 #cp autostart_node.sh /etc/rc3.d
 
 echo "reboot in 10 seconds..."
+sleep 10
+sudo reboot
 
-#sleep(10)
 
